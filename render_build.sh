00f6ec999ed5e28bd2b0e1bf14ac0877a507c8d8
prongselk <prongselk@gmail.com>
2025-02-05 22:17:05 +0000

    workaround for basemap

---
+++ render_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Download and install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Initialize Conda and install dependencies
+conda init
+conda create -n myenv python=3.9 -y
+conda activate myenv
+conda install -c conda-forge basemap -y
+
+# Install pip dependencies
+pip install -r requirements.txt
