8235a8a528f3dac6900ae6ccd8c282cb9b6e8396
prongselk <prongselk@gmail.com>
2025-02-05 21:14:40 +0000

    error fix

--- render-build.sh
+++ render-build.sh
@@ -6,12 +6,15 @@ apt-get update
 # Install system dependencies required for ipopt and cyipopt
 apt-get install -y \
     pkg-config \
-    libipopt-dev \
+    coinor-libipopt-dev \
     libblas-dev \
     liblapack-dev \
     gfortran \
     build-essential \
     python3-dev
+
+# Set PKG_CONFIG_PATH to help pip find ipopt
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 
 # Upgrade pip
 pip install --upgrade pip
