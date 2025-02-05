3c4c0912c80a47419ac9864e9077a967efd077c8
prongselk <prongselk@gmail.com>
2025-02-05 21:06:49 +0000

    more dependicies

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 # Install system dependencies
-apt-get update && apt-get install -y pkg-config libipopt-dev
+apt-get update && apt-get install -y \
+    pkg-config \
+    libipopt-dev \
+    build-essential \
+    python3-dev
+
+# Upgrade pip
+pip install --upgrade pip
 
 # Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
