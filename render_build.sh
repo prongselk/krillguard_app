ec1538dc7e1bf22edfc48dda959e5c95e82f1c98
prongselk <prongselk@gmail.com>
2025-02-06 16:59:42 +0000

    fixing conda init

--- render_build.sh
+++ render_build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-set -e  # Exit on error
+set -e  # Exit script if any command fails
 
 echo "Initializing Conda..."
-source $HOME/miniconda/etc/profile.d/conda.sh || true 
-conda init || true 
+export PATH="$HOME/miniconda/bin:$PATH"
+source $HOME/miniconda/etc/profile.d/conda.sh || true
+conda init bash
 
 echo "Creating Conda environment..."
 conda create -n myenv python=3.9 -y
+
+# Reinitialize Conda environment
+echo "Reinitializing Conda..."
+source $HOME/miniconda/etc/profile.d/conda.sh
 conda activate myenv
 
-echo "Installing dependencies with Conda..."
-conda install -n myenv -c conda-forge \
-    pandas numpy matplotlib basemap openpyxl -y
+echo "Installing Conda dependencies..."
+conda install -n myenv -c conda-forge pandas numpy matplotlib basemap openpyxl -y
 
 echo "Installing remaining dependencies with pip..."
 pip install dash flask plotly gunicorn
